87146d5fde8ade9bc108974d9345661f6693394d
Amy Lee <amyleeannebennett@gmail.com>
2016-04-25 09:07:00 -0500

    routes are working in config directory

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  Rails.application.routes.draw do
 
+    resources :articles
+
+    root 'welcome#index'
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -13,6 +19,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
 
   # Example resource route with options:
   #   resources :products do
